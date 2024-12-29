b0e495d9885db8fd929e1192ef5e81c3ae24bcfd
Karan <karanbishtkb2922@gmail.com>
2024-12-30 01:33:12 +0530

    done

---
+++ Frontend/src/components/About.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Navbar from './Navbar'
+import Aboutinfo from './aboutinfo';
+import { Link } from "react-router-dom";
+import Cards from "./Cards";
+
+function About(){
+  return (
+    <>
+    <Navbar/>
+    <Aboutinfo />
+   
+    </>
+  )
+}
+
+export default About;
